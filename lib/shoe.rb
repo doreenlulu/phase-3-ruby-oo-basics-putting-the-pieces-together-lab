b9342ab9414418d148d10e7b0ca69cd58585995f
Doreen <doreenlulu97@gmail.com>
2022-10-22 00:16:21 +0300

    Completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand 
+    end
+
+    def cobble
+        #this says that the shoe has been repaired
+        puts "Your shoe is as good as new!"
+        #Makes the shoe's condition to new
+        @condition = "new"
+    end
+end
